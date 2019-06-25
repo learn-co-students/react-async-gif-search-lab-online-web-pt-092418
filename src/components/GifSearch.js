acd2d1e7068d28949ecb9c29a31328b31b7593ef
github username <github email address>
2019-06-25 17:16:04 -0400

    wip - create skeleton of GifSearch.

---
+++ src/components/GifSearch.js
@@ -0,0 +1,15 @@
+import GifListContainer from '../containers/GifListContainer';
+
+import React, { Component } from 'react';
+
+  class GitSearch extends Component {
+
+    render() {
+      return (
+        <div></div>
+      );
+    }
+
+  }
+
+export default GitSearch;
