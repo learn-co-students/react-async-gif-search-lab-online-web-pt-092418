9875de2f36fd1aed3553c61be04cd7d020bc0200
github username <github email address>
2019-06-25 21:25:17 -0400

    wip - add searchGif callback function in GifListContainer

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GifList from '../components/GifList';
+import GifSearch from '../components/GifSearch';
 
   class GifListContainer extends Component {
     state = {
@@ -9,6 +10,7 @@ import GifList from '../components/GifList';
     render() {
       return (
         <div>
+          <GifSearch />
           <GifList gifsArr = {this.state.gifsArr}/>
         </div>
       )
@@ -21,6 +23,14 @@ import GifList from '../components/GifList';
         gifsArr: json.data.map(gif => ({url: gif.images.original.url}))
       }))
     }
+
+    searchGif = (query) => {
+      fetch("http://api.giphy.com/v1/gifs/search?q={query}&api_key=dc6zaTOxFJmzC&rating=g")
+    }
+    .then(res => res.json())
+    .then(json => this.setState({
+      gifsArr: json.data.map(gif => ({url: gif.images.original.url}))
+    }))
 
   }
 
