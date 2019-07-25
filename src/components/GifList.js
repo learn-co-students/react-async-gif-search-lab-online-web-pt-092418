81aac5a22ee5f98d52eeebbb49afcd4775ea207c
Carolyn Whelpley <carolynwhelpley@Carolyns-MacBook-Air.local>
2019-07-25 11:08:28 -0400

    Interim checkin

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 class GifList extends Component{ 
     render(){
     const list = this.props.gifs.map((gif, i) => {
-      return(  <li key={gif.id}><img src={gif.url} alt="great gifs"/> </li>
+      return(  <li key={gif.id}><img src={gif.images.original.url} alt="great gifs"/> </li>
     )});
         return (
             <ul>
--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -4,18 +4,26 @@ import React, {Component} from 'react';
 class GifSearch extends Component{ 
     
         state = {
-          
+          value: '',
         }
         handleSubmit = event => {
             this.setState({
                 value: event.target.value
             });
+        handleInputChange = event => {
+            this.setState({
+                search: event.target.value
+            });
+        }
         }
         render() {
           return (
             <form onSubmit={ event => this.handleSubmit(event) }>
-              
-              <input type="text"  />
+              Enter Search term:
+              <input type="text" id="search"
+                value={this.state.value}
+                onChange={event => this.handleInputChange(event)}
+              />
             </form>
           )
         }
